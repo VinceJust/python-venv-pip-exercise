752430f46c8bc58e803db2f24c885f4141f82063
hitze <hitze1126@googlemail.com>
2024-10-08 13:13:39 +0200

    script erstellt

--- data_visualization.py
+++ data_visualization.py
@@ -1,19 +1,28 @@
 import matplotlib.pyplot as plt
 
-# Beispiel-Daten: Zahlen von 0 bis 9 und ihre Quadrate
+# Beispiel-Daten: Zahlen von 0 bis 9
 x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-y = [i**2 for i in x]  # Quadratzahlen
 
-# Liniendiagramm erstellen
-plt.plot(x, y, label='Quadratzahlen')
+# Berechnung der Quadratzahlen und Kubikzahlen
+quadratzahlen = [i**2 for i in x]
+kubikzahlen = [i**3 for i in x]
+
+# Liniendiagramm für Quadratzahlen erstellen
+plt.plot(x, quadratzahlen, label='Quadratzahlen', color='blue', marker='o')
+
+# Liniendiagramm für Kubikzahlen erstellen
+plt.plot(x, kubikzahlen, label='Kubikzahlen', color='red', marker='x')
 
 # Titel und Achsenbeschriftungen hinzufügen
-plt.title('Quadratzahlen von 0 bis 9')
+plt.title('Quadratzahlen und Kubikzahlen von 0 bis 9')
 plt.xlabel('Zahl')
-plt.ylabel('Quadrat')
+plt.ylabel('Wert')
 
 # Legende anzeigen
 plt.legend()
 
-# Diagramm anzeigen
-plt.show()
+# Diagramm speichern
+plt.savefig('quadratzahlen_kubikzahlen.png')
+
+# Optional: Diagramm anzeigen
+# plt.show()
