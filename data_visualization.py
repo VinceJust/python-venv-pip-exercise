43ec93e54e5e6ff16d129274794a2a23824c73d0
VinJust <vpj97@outlook.de>
2024-10-08 15:10:39 +0200

    added data visualization script using matplotlib and pandas

--- data_visualization.py
+++ data_visualization.py
@@ -1,13 +1,24 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # Matplotlib import
+import pandas as pd # Panda import
 
 # Zahlen von 0 bis 9
 x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # Berechnung der Zahlen
-quadratzahlen = [i**2 for i in x]
-kubikzahlen = [i**3 for i in x]
+quadratzahlen = [i**2 for i in x]  # Quadratzahlen
+kubikzahlen = [i**3 for i in x]     # Kubikzahlen
 
-# LDiagramm für Quadratzahlen
+# DataFrame mit Pandas
+daten = pd.DataFrame({
+    'Zahl': x,
+    'Quadratzahl': quadratzahlen,
+    'Kubikzahl': kubikzahlen
+})
+
+# Speichern des DataFrames in einer CSV-Datei
+daten.to_csv('zahlen.csv', index=False)
+
+# Diagramm für Quadratzahlen
 plt.plot(x, quadratzahlen, label='Quadratzahlen', color='blue', marker='o')
 
 # Diagramm für Kubikzahlen
@@ -21,8 +32,8 @@ plt.ylabel('Wert')
 # Legende anzeigen
 plt.legend()
 
-# speichern als Bild
-plt.savefig('quadratzahlen_kubikzahlen.png')
+# Bild gespeichert als PNG mit einer dpi 300 Auflösung
+plt.savefig('quadratzahlen_kubikzahlen.png', format='png', dpi=300)
 
 # Diagramm anzeigen
-# plt.show()
+plt.show()
